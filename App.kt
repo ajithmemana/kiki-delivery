7f670ce490431fcece8793aefb7556ccf7bc2ce4
Ajith <ajithmemana@outlook.com>
2024-05-29 09:46:29 +0530

    Added methods to compute delivery time

--- App.kt
+++ App.kt
@@ -1,6 +1,14 @@
 package com.example.kikicouriers.models
 
-// App.kt
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
+// Argument specs for runnable file, used for separating and validating input arguments
+const val NUM_OF_LEADING_ARGS = 2
+const val NUM_OF_ARGS_PER_PACKAGE = 4
+const val NUM_OF_TRAILING_ARGS = 3
+
 data class Offer(
     val code: String,
     val discount: Double,
@@ -9,6 +17,18 @@ data class Offer(
     val minWeight: Int,
     val maxWeight: Int
 )
+
+data class Package(
+    val id: String,
+    val weight: Int,
+    val distance: Int,
+    val offerCode: String?,
+    val offerDiscount: Int = 0,
+    val deliveryCost: Int = 0,
+    var timeTaken: Double = 0.0,
+    var deliveredAt: Double = 0.0
+)
+
 
 // If there are more valid offer codes, add here to the list
 val offers = listOf(
@@ -19,17 +39,21 @@ val offers = listOf(
 
 fun main(args: Array<String>) {
 
-    if (args.size < 6) {
+    if (args.size < 9) {
         println(
             "Usage: [base_delivery_cost] [no_of_packges]\n" +
-                    "[pkg_id1] [pkg_weight1_in_kg] [distance1_in_km] [offer_code1]"
+                    "[pkg_id1] [pkg_weight1_in_kg] [distance1_in_km] [offer_code1] [no_of_vehicles] [max_speed] [max_carriable_weight]"
         )
         return
     }
-
+    // Extract arguments
     val baseDeliveryCost = args[0].toIntOrNull()
     val noOfPackages = args[1].toIntOrNull()
+    val noOfVehicles = args[args.size - 3].toIntOrNull()
+    val maxSpeed = args[args.size - 2].toIntOrNull()
+    val maxAllowedWeight = args[args.size - 1].toIntOrNull()
 
+    // Do basic validations and return if cannot proceed further
     if (baseDeliveryCost == null) {
         println("Base delivery cost cannot be null")
         return
@@ -38,24 +62,30 @@ fun main(args: Array<String>) {
         println("Please input details of at least one package")
         return
     }
-    if (args.size != (2 + (4 * noOfPackages))) {
+    if (noOfVehicles == null || maxSpeed == null || maxAllowedWeight == null) {
+        println("At least one vehicle info is required for delivery")
+        return
+    }
+    if (args.size != (NUM_OF_LEADING_ARGS + (NUM_OF_ARGS_PER_PACKAGE * noOfPackages) + NUM_OF_TRAILING_ARGS)) {
         println("Provided data doesn't match number of packages")
         return
     }
-
-    val packages = args.copyOfRange(2, args.size).asList().chunked(4)
-
-    packages.forEach { it ->
-        val calculatedCosts =
-            getDiscountAndDeliveryCost(
-                baseDeliveryCost,
-                it[1].toInt(),
-                it[2].toInt(),
-                it[3],
-                offers
-            )
-        println("${it[0]}  ${calculatedCosts.first} ${calculatedCosts.second}")
+    // Split data into an array of packages
+    val packages = args.copyOfRange(2, args.size - 3).asList().chunked(4).map {
+        Package(it[0], it[1].toInt(), it[2].toInt(), it[3])
     }
+
+    val costEstimatedPackages = estimateCosts(packages, baseDeliveryCost)
+
+    val timeCalculatedPackages =
+        calculateDeliveryTime(costEstimatedPackages, noOfVehicles, maxSpeed, maxAllowedWeight)
+
+    timeCalculatedPackages.forEach { pkg ->
+        val decimalFormat = DecimalFormat("#.00")
+        println("${pkg.id} ${pkg.offerDiscount} ${pkg.deliveryCost} ${decimalFormat.format(pkg.deliveredAt)}")
+//        println("${pkg.id} ${pkg.offerDiscount} ${pkg.deliveryCost} ${decimalFormat.format(pkg.deliveredAt)}")
+    }
+
 }
 
 /**
@@ -75,3 +105,83 @@ fun getDiscountAndDeliveryCost(
     return (discount * totalCost).toInt() to (totalCost * (1 - discount)).toInt()
 }
 
+/**
+ * Estimate the cost of delivering each package in a list of packages.
+ * Returns list of package with discount and final cost estimate values applied
+ */
+fun estimateCosts(packages: List<Package>, baseDeliveryCost: Int): List<Package> {
+    return packages.map { pkg ->
+        val costs = getDiscountAndDeliveryCost(
+            baseDeliveryCost,
+            pkg.weight,
+            pkg.distance,
+            pkg.offerCode,
+            offers
+        )
+        pkg.copy(offerDiscount = costs.first, deliveryCost = costs.second)
+    }
+}
+
+/**
+ * Returns a set of packages sorted by their total weights, in a descending order
+ */
+fun createDeliverySets(packages: List<Package>, maxSum: Int): List<List<Package>> {
+    val sortedPackages = packages.sortedByDescending { it.weight }
+    val sets = mutableListOf<MutableList<Package>>()
+
+    sortedPackages.forEach { pkg ->
+        val set = sets.find { it.sumOf { it.weight } + pkg.weight <= maxSum }
+        if (set != null) {
+            set.add(pkg)
+        } else {
+            sets.add(mutableListOf(pkg))
+        }
+    }
+    return sets.sortedByDescending { it.sumOf { it.weight } }
+}
+
+/**
+ * Compute the deliveries to maximize efficiency based on the given vehicle constraints.
+ * Returns a package with time taken and delivered times applied
+ *
+ * Logic:
+ * while packages set is non empty{
+ *      pick the vehicle with lowest current time
+ *      pick the heaviest set from packages
+ *      Calculate time for each items in a delivery set - timeTaken, delivered at (Current time of vehicle + timeTaken)
+ *      Update current time of vehicle (it + Time taken for farthest package x 2)
+ *      Removed the delivered item from top of list
+ * }
+ */
+fun calculateDeliveryTime(
+    packages: List<Package>,
+    numOfVehicles: Int,
+    maxSpeed: Int,
+    maxAllowedWeight: Int
+): List<Package> {
+
+    val packageSetsToBeDelivered = createDeliverySets(packages, maxAllowedWeight).toMutableList()
+
+    val vehicleTimer = DoubleArray(numOfVehicles) { 0.0 }
+    var packageSetIndex = 0
+
+    while (packageSetIndex < packageSetsToBeDelivered.size) {
+        for (vehicle in 0 until numOfVehicles) {
+            val firstAvailableVehicleTimer = vehicleTimer.minOrNull()!!     // numOfVehicles will be >0
+            val firstAvailableVehicleIndex =
+                vehicleTimer.indexOfFirst { it == firstAvailableVehicleTimer }
+
+            packageSetsToBeDelivered.first().forEach() {
+                it.timeTaken = BigDecimal(it.distance.toDouble() / maxSpeed).setScale(2, RoundingMode.FLOOR).toDouble()
+                it.deliveredAt = BigDecimal(firstAvailableVehicleTimer + it.timeTaken).setScale(2, RoundingMode.HALF_UP).toDouble()
+                println("Delivered ${it.id} at ${it.deliveredAt}")
+            }
+            val farthestPackage = packageSetsToBeDelivered.first().maxBy { it.timeTaken }
+            vehicleTimer[firstAvailableVehicleIndex] =
+                vehicleTimer[firstAvailableVehicleIndex] + 2 * farthestPackage.timeTaken
+            packageSetsToBeDelivered.removeAt(0)
+        }
+        packageSetIndex++
+    }
+    return packages
+}
--- TestMain.kt
+++ TestMain.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 
 class TestMain {
 
+    // Offer codes
     @Test
     fun testCase1() {
         val baseCost = 100
@@ -13,7 +14,6 @@ class TestMain {
         val offerCode = "OFR001"
 
         val result = getDiscountAndDeliveryCost(baseCost, weight, distance, offerCode, offers)
-
         assertEquals(0 to 175, result)
     }
 
@@ -25,7 +25,6 @@ class TestMain {
         val offerCode: String? = "OFR003"
 
         val result = getDiscountAndDeliveryCost(baseCost, weight, distance, offerCode, offers)
-
         assertEquals(35 to 665, result)
     }
 
@@ -41,5 +40,50 @@ class TestMain {
         assertEquals(0 to 2100, result)
     }
 
+    // Delivery time
+
+    @Test
+    fun testCreateDeliverySets() {
+        val packages = listOf(
+            Package("PKG1", 50, 150, "OFR002"),
+            Package("PKG2", 75, 200, null),
+            Package("PKG3", 175, 100, "OFR001"),
+            Package("PKG4", 110, 50, null),
+            Package("PKG5", 155, 50, "OFR001")
+        )
+        val maxSum = 200
+        val expected = listOf(
+            listOf(packages[3], packages[1]),
+            listOf(packages[2]),
+            listOf(packages[4]),
+            listOf(packages[0])
+        )
+        val result = createDeliverySets(packages, maxSum)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testCalculateDeliveryTime() {
+        val packages = listOf(
+            Package("PKG1", 50, 30, "OFR002"),
+            Package("PKG2", 75, 125, "OFR008"),
+            Package("PKG3", 175, 100, "OFR003"),
+            Package("PKG4", 110, 60, "OFR002"),
+            Package("PKG5", 155, 95, "NA"),
+        )
+        val numOfVehicles = 2
+        val maxSpeed = 70
+        val maxAllowedWeight = 200
+
+        val expected = listOf(
+            packages[0].copy(deliveredAt = 3.98, timeTaken = 0.42),
+            packages[1].copy(deliveredAt = 1.78, timeTaken = 1.78),
+            packages[2].copy(deliveredAt = 1.42, timeTaken = 1.42),
+            packages[3].copy(deliveredAt = 0.85, timeTaken = 0.85),
+            packages[4].copy(deliveredAt = 4.19, timeTaken = 1.35),
+        )
+        val result = calculateDeliveryTime(packages, numOfVehicles, maxSpeed, maxAllowedWeight)
+        assertEquals(expected, result)
+    }
 
 }
